3886a6af97f325e6165066563bec550917a771db
Dimon Hp-pc <dimon@hp-pc.home>
2024-08-13 10:07:42 +0300

    Изменения, которые будут включены в коммит: 	новый файл:    you-git.sh

---
+++ you-git.sh
@@ -0,0 +1,11 @@
+!#/bin/bash
+
+cd
+
+username=`whoami`
+hostname=`hostname -s`
+
+git config --global user.name "${username^} ${hostname^}"
+git config --global user.email ${username}@$(hostname)
+
+[ -d "you-git" ] || git clone git@github.com:madixi/you-git.git
