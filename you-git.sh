4ca39b3dc0bb77ddac28b1707e26a8d177478c14
Dimon Hp-pc <dimon@hp-pc.home>
2024-08-13 14:38:20 +0300

    dpkg

--- you-git.sh
+++ you-git.sh
@@ -17,11 +17,11 @@ git pull
 cp you_git.py ../1you-git/
 cd
 
-if [ ! dpkg -l yandex-disk > /dev/null 2>&1 ]; then
+dpkg -l yandex-disk > /dev/null 2>&1 || (
     sudo apt-get install wget gpg
     wget -qO- http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG | gpg --dearmor > yandex-disk.gpg
     sudo install -D -o root -g root -m 644 yandex-disk.gpg /etc/apt/keyrings/yandex-disk.gpg
     echo "deb [signed-by=/etc/apt/keyrings/yandex-disk.gpg] http://repo.yandex.ru/yandex-disk/deb/ stable main" | sudo tee /etc/apt/sources.list.d/yandex-disk.list > /dev/null
     rm yandex-disk.gpg
     sudo apt install -y yandex-disk
-fi
+)
