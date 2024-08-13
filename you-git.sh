8d21c10d8adf20458bd2b8e6b85ca326ad5ed6c8
Dimon Hp-pc <dimon@hp-pc.home>
2024-08-13 10:52:14 +0300

    you-git.sh

--- you-git.sh
+++ you-git.sh
@@ -5,7 +5,12 @@ cd
 username=`whoami`
 hostname=`hostname -s`
 
-git config --global user.name "${username^} ${hostname^}"
-git config --global user.email ${username}@$(hostname)
+git config --global user.name >/dev/null || git config --global user.name "${username^} ${hostname^}"
+git config --global user.name >/dev/null || git config --global user.email ${username}@$(hostname)
 
 [ -d "you-git" ] || git clone git@github.com:madixi/you-git.git
+
+mkdir -p 1you-git
+cd 1you-git
+cp ../you-git/you-git.py
+cd
